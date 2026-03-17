1c84a763d41e521d281ab5119f869955ef06d97c
Prachiti Kitey <prachitikitey86@gmail.com>
2026-03-17 13:51:25 +0530

    add parser

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include<iostream>
+#include<sstream>
 #include<string>
+#include<vector>
 
 using namespace std;
+
+vector<string> parse(const string& input){
+    vector<string> args;
+    stringstream ss(input);
+    string word;
+
+    while(ss>>word){
+        args.push_back(word);
+    }
+    return args;
+}
 
 int main(){
     while(true){
@@ -9,11 +22,14 @@ int main(){
         cout<<"my_shell> "<<flush;
 
         if(!getline(cin, input)) break;
-
         if(input.empty()) continue;
-
         if(input=="exit") break;
 
-        cout<<input<<endl;
+        vector<string> tokens = parse(input);
+
+        for(const auto& token:tokens){
+            cout<<"["<<token<<"]";
+        }
+        cout<<endl;
     }
 }
