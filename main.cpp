32173c519a54ebf92a8f3d4d0d2e875b9292722f
Prachiti Kitey <prachitikitey86@gmail.com>
2026-03-17 14:54:52 +0530

    add os commands

--- main.cpp
+++ main.cpp
@@ -2,8 +2,36 @@
 #include<sstream>
 #include<string>
 #include<vector>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<cstdlib>
 
 using namespace std;
+
+void execute(vector<char*>& args){
+    pid_t pid = fork();
+    if(pid == 0){
+        execvp(args[0], args.data());
+        perror("execvp failed");
+        exit(1);
+    }
+    else if(pid>0){
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    else{
+        perror("fork failed");
+    }
+}
+
+vector<char*> toCharArray(vector<string>& args){
+    vector<char*> res;
+    for(auto& s:args){
+        res.push_back(const_cast<char*>(s.c_str()));
+    }
+    res.push_back(nullptr);
+    return res;
+}
 
 vector<string> parse(const string& input){
     vector<string> args;
@@ -26,10 +54,9 @@ int main(){
         if(input=="exit") break;
 
         vector<string> tokens = parse(input);
+        if (tokens.empty()) continue;
 
-        for(const auto& token:tokens){
-            cout<<"["<<token<<"]";
-        }
-        cout<<endl;
+        vector<char*> args = toCharArray(tokens);
+        execute(args);
     }
 }
