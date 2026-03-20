8a228be4da47075d6bc90b13526c84033139b840
Prachiti Kitey <prachitikitey86@gmail.com>
2026-03-20 11:10:39 +0530

    add cd command

--- main.cpp
+++ main.cpp
@@ -1,62 +1,112 @@
-#include<iostream>
-#include<sstream>
-#include<string>
-#include<vector>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
 using namespace std;
 
-void execute(vector<char*>& args){
+void execute(vector<char *> &args)
+{
     pid_t pid = fork();
-    if(pid == 0){
+    if (pid == 0)
+    {
         execvp(args[0], args.data());
         perror("execvp failed");
         exit(1);
     }
-    else if(pid>0){
+    else if (pid > 0)
+    {
         int status;
         waitpid(pid, &status, 0);
     }
-    else{
+    else
+    {
         perror("fork failed");
     }
 }
 
-vector<char*> toCharArray(vector<string>& args){
-    vector<char*> res;
-    for(auto& s:args){
-        res.push_back(const_cast<char*>(s.c_str()));
+vector<char *> toCharArray(vector<string> &args)
+{
+    vector<char *> res;
+    for (auto &s : args)
+    {
+        res.push_back(const_cast<char *>(s.c_str()));
     }
     res.push_back(nullptr);
     return res;
 }
 
-vector<string> parse(const string& input){
+vector<string> parse(const string &input)
+{
     vector<string> args;
     stringstream ss(input);
     string word;
 
-    while(ss>>word){
+    while (ss >> word)
+    {
         args.push_back(word);
     }
     return args;
 }
 
-int main(){
-    while(true){
-        string input;
-        cout<<"my_shell> "<<flush;
+bool handleBuiltins(vector<string> &tokens)
+{
+    if (tokens[0] == "exit")
+    {
+        exit(0);
+    }
 
-        if(!getline(cin, input)) break;
-        if(input.empty()) continue;
-        if(input=="exit") break;
+    if (tokens[0] == "cd")
+    {
+        if (tokens.size() < 2)
+        {
+            const char *home = getenv("HOME");
+            if (home == nullptr)
+            {
+                cerr << "cd: Home not set\n";
+            }
+            else
+            {
+                if (chdir(home) != 0)
+                {
+                    perror("cd failed");
+                }
+            }
+        }
+        else
+        {
+            if (chdir(tokens[1].c_str()) != 0)
+            {
+                perror("cd failed");
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    while (true)
+    {
+        string input;
+        cout << "my_shell> " << flush;
+
+        if (!getline(cin, input))
+            break;
+        if (input.empty())
+            continue;
 
         vector<string> tokens = parse(input);
-        if (tokens.empty()) continue;
+        if (tokens.empty())
+            continue;
 
-        vector<char*> args = toCharArray(tokens);
+        if(handleBuiltins(tokens)) continue;
+
+        vector<char *> args = toCharArray(tokens);
         execute(args);
     }
 }
